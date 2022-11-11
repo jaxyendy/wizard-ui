18cfcf6ae8979ec790fb4d8c8777afa36ba30d69
Fabricio C Zuardi <fabricio@fabricio.org>
2022-11-11 18:52:10 -0300

    feat(intl): basic i18n setup

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ['pt-BR'],
+    defaultLocale: 'pt-BR',
+  },
 }
 
 module.exports = nextConfig
